6b3f9c09c71611cd621e2b4cb771133a4305038f
caullens <caullens@ksu.edu>
2017-04-26 15:11:23 -0500

    Mid working state, everything has to change though

--- public/login.js
+++ public/login.js
@@ -1,5 +1,26 @@
+"use strict;"
+
+var encryption = require('../lib/encryption.js');
+
 $(function() {
-  $("#button-login").on('click', function() {
-    console.log("CLICKED");
+  $("#button-login").on('click', function(event) {
+    event.preventDefault();
+    var name = $('#user-field').val();
+    var pass = $('#pass-field').val();
+    if(user == '' && pass == 'simple'){
+      // matching password & username - log the user in
+      // by creating the session object
+      var session = {username: username};
+      // JSON encode the session object
+      var sessionData = JSON.stringify(session);
+      // Encrypt the session data
+      var sessionCrypt = encryption.encipher(sessionData);
+      // And send it to the client as a session cookie
+      res.setHeader("Set-Cookie", ["cryptsession=" + sessionCrypt + "; session;"]);
+      // Finally, redirect back to the index
+      res.statusCode = 302;
+      res.setHeader("Location", "/index.html");
+      res.end();
+    }
   });
 });
--- server.js
+++ server.js
@@ -5,39 +5,23 @@
  */
 "use strict;"
 
+var port = 4000;
+
 /* global variables */
-var multipart = require('./multipart');
-var template = require('./template');
-var staticFiles = require('./static');
+var multipart = require('./lib/multipart');
+var template = require('./lib/template');
+var fileserver = require('./lib/fileserver');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var port = 4000;
-
-/* load cached files */
-var config = JSON.parse(fs.readFileSync('config.json'));
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('chirp.sqlite3');
 
 /* load public directory */
-staticFiles.loadDir('public');
+fileserver.loadDir('public');
 
 /* load templates */
 template.loadDir('templates');
-
-/** @function getPioneerFilenames
- * Retrieves the filenames for all images in the
- * /pioneers directory and supplies them to the callback.
- * @param {function} callback - function that takes an
- * error and array of filenames as parameters
- */
-function getPioneerFilenames(callback) {
-  fs.readdir('pioneers/', function(err, filenames){
-    if(err) return callback(err, undefined);
-    var filenamesWithDir = filenames.map(function(filename){
-      return 'pioneers/' + filename;
-    });
-    callback(false, filenamesWithDir);
-  });
-}
 
 /** @function parseFiles
  * Asynchronous gelper function that takes an array of JSON
@@ -69,47 +53,6 @@ function parseFiles(filenames, callback) {
   });
 }
 
-/** @function getPioneerTags
- * A helper function to create anchor tags for each
- * computing pioneer with a JSON file in the /pioneers
- * directory.
- * The callback should accept an error and the parsed
- * JSON data as an array.
- * @param {function} callback - the callback function
- */
-function getPioneerTags(callback) {
-  getPioneerFilenames(function(err, filenames) {
-    if(err) return callback(err);
-    parseFiles(filenames, function(err, pioneers){
-      if(err) return callback(err);
-      var pioneerTags = pioneers.map(function(pioneer) {
-        return template.render('pioneerTag.html', pioneer);
-      }).join("");
-      callback(false, pioneerTags);
-    });
-  });
-}
-
-/** @function serveIndex
- * A function to serve a HTML page representing an
- * index of computer science pioneers.
- * @param {http.incomingRequest} req - the request object
- * @param {http.serverResponse} res - the response object
- */
-function serveIndex(req, res) {
-  getPioneerTags(function(err, tags){
-    if(err) {
-      console.error(err);
-      res.statusCode = 500;
-      res.statusMessage = 'Server error';
-      res.end();
-      return;
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.end(template.render('index.html', {pioneerTags: tags}));
-  });
-}
-
 /** @function serveLogin
  * A function to serve a HTML page representing an
  * index of computer science pioneers.
@@ -119,6 +62,11 @@ function serveIndex(req, res) {
 function serveLogin(req, res) {
     res.setHeader('Content-Type', 'text/html');
     res.end(template.render('login.html'));
+}
+
+function serveCreate(req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.end(template.render('create-account.html'));
 }
 
 /** @function serveImage
@@ -142,87 +90,6 @@ function serveImage(fileName, req, res) {
   });
 }
 
-/** @function servePioneer
- * A function to serve a computing pioneer's JSON data.
- * @param {string} filename - the JSON filename
- * to serve.
- * @param {http.incomingRequest} - the request object
- * @param {http.serverResponse} - the response object
- */
-function servePioneer(filename, req, res) {
-  fs.readFile('pioneers/' + filename + '.json', function(err, data) {
-    if(err) {
-      console.error(err);
-      res.statusCode = 500;
-      res.statusMessage = "Server error";
-      return;
-    }
-    var pioneer = JSON.parse(data);
-    res.setHeader('Content-Type', 'text/html');
-    res.end(template.render('pioneer.html', pioneer));
-  })
-}
-
-/** @function uploadPioneer
- * A function to process an http POST request
- * containing a new computing pioneer to add to the collection.
- * @param {http.incomingRequest} req - the request object
- * @param {http.serverResponse} res - the response object
- */
-function uploadPioneer(req, res) {
-  multipart(req, res, function(req, res) {
-    // make sure an image was uploaded
-    if(!req.body.image.filename) {
-      console.error("No file in upload");
-      res.statusCode = 400;
-      res.statusMessage = "No file specified"
-      res.end("No file specified");
-      return;
-    }
-    // make sure image was in jpeg format
-    if(req.body.image.contentType != 'image/jpeg'){
-      res.statusCode = 400;
-      res.statusMessage = "Must be type image/jpeg";
-      res.end("Your image file must be a JPEG");
-      return;
-    }
-    // Write the image
-    fs.writeFile('images/' + req.body.image.filename, req.body.image.data, function(err){
-      if(err) {
-        console.error(err);
-        res.statusCode = 500;
-        res.statusMessage = "Server Error";
-        res.end("Server Error");
-        return;
-      }
-      // Determine the id and advance the counter
-      var id = config.entryCount;
-      config.entryCount++;
-      fs.writeFile('config.json', JSON.stringify(config));
-      // Populate a pioneer object
-      var pioneer = {
-        id: id,
-        name: req.body.name,
-        description: req.body.description,
-        imageUrl: req.body.image.filename
-      }
-      // Write the JSON file
-      fs.writeFile('pioneers/' + id + '.json', JSON.stringify(pioneer), function(err){
-        if(err) {
-          console.error(err);
-          req.statusCode = 500;
-          req.statusMessage = "Server error";
-          req.end("Unable to create pioneer");
-          return;
-        }
-        // If we reach this point, everything is saved,
-        // so serve the updated index.
-        serveIndex(req, res);
-      });
-    });
-  });
-}
-
 /** @function handleRequest
  * A function to determine what to do with
  * incoming http requests.
@@ -236,6 +103,7 @@ function handleRequest(req, res) {
     // Simplest case is the user requests the index
     // or default page.
     case '':
+    case 'home':
       if(req.method == 'GET') {
         serveIndex(req, res);
       } else if(req.method == 'POST') {
@@ -250,11 +118,14 @@ function handleRequest(req, res) {
     case 'user':
       req.params.user = urlParts[2];
       serveUser(req, res);
+    case 'create-account':
+      if(req.method == 'GET') serveCreate(req, res);
+      break;
     default:
       // Check if the request is for a file in the
       // public directory
-      if(staticFiles.isCached('public' + req.url)) {
-        staticFiles.serveFile('public' + req.url, req, res);
+      if(fileserver.isCached('public' + req.url)) {
+        fileserver.serveFile('public' + req.url, req, res);
       }
       else {
         // Otherwise, we have three possibilities -
